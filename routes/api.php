5b5107f4757d9b9a2d482bd4dfebe039acc707f4
yuman211 <yukinuma0211@gmail.com>
2022-04-18 21:19:31 +0900

    route修正

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\GameController;
-use App\Models\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
