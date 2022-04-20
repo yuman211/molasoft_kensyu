f2383d5b86bde9b66f73e3a77700324ee972ee04
yuman211 <yukinuma0211@gmail.com>
2022-04-20 21:07:27 +0900

    trycatch追加

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,20 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Game;
+use Illuminate\Support\Facades\Log;
+use Exception;
 
 class GameController extends Controller
 {
     public function showAllGames(Game $game)
     {
-        return $game->getAllGamesWithTeams();
+        try {
+            return $game->getAllGamesWithTeams();
+        } catch (Exception $e) {
+            Log::emergency($e->getMessage());
+            return $e;
+        }
     }
 
     public function searchGames(Game $game,Request $request)
     {
-        $game_date = $request->input('date');
-        $game_location = $request->input('location');
-        $genre = $request->input('genre');
-        
-        return $game->getLocatedGamesWithTeams($game_date,$game_location,$genre);
+        try {
+            $game_date = $request->input('date');
+            $game_location = $request->input('location');
+            $genre = $request->input('genre');
+
+            return $game->getLocatedGamesWithTeams($game_date, $game_location, $genre);
+        } catch (Exception $e) {
+            Log::emergency($e->getMessage());
+            return $e;
+        }
     }
 }
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+use Exception;
 
 class Game extends Model
 {
@@ -16,25 +18,27 @@ class Game extends Model
 
     public function getAllGamesWithTeams()
     {
-        
+
         return $this->with('teams')->get();
     }
 
     public function getLocatedGamesWithTeams($game_date=null,$game_location=null,$genre=null)
     {
-        $query = $this->query();
-        if(isset($game_date))
-        {
-            $query->where('date',$game_date);
+        try {
+            $query = $this->query();
+            if (isset($game_date)) {
+                $query->where('date', $game_date);
+            }
+            if (isset($game_location)) {
+                $query->where('location', $game_location);
+            }
+            if (isset($genre)) {
+                $query->where('genre', $genre);
+            }
+            return $query->with('teams')->get();
+        } catch (Exception $e) {
+            Log::emergency($e->getMessage());
+            throw $e;
         }
-        if(isset($game_location))
-        {
-            $query->where('location', $game_location);
-        }
-        if(isset($genre))
-        {
-            $query->where('genre', $genre);
-        }
-        return $query->with('teams')->get();
     }
 }
